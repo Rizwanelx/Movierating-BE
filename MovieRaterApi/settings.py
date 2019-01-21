a573e3bc9d4c5ccbd065b4911bb912475557b29d
Rizwan Khan <rizwanelx@gmail.com>
2019-01-21 12:49:30 +0530

    make it better

--- MovieRaterApi/settings.py
+++ MovieRaterApi/settings.py
@@ -16,7 +16,6 @@ from dj_database_url import parse as dburl
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media', )
 
 
 # Quick-start development settings - unsuitable for production
